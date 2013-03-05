3f20a49b89c1913cfa8d392e0eddd1bda27c65c1
Raj Gokal <raj@sano.co>
2013-03-04 17:02:08 -0800

    adds some buttons

--- Mental Calculator/CalcFirstViewController.m
+++ Mental Calculator/CalcFirstViewController.m
@@ -27,7 +27,54 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    // creating custom button properties
+    UIFont *buttonFont = [UIFont fontWithName:@"Gotham Medium" size:17.0];
+    UIColor *buttonColorDefault = [UIColor blackColor];
+    UIColor *buttonColorHighlight = [UIColor blackColor];
+    UIImage *btn = [UIImage imageNamed:@"Button.png"];
+    UIImage *btnh = [UIImage imageNamed:@"ButtonHighlighted.png"];
+    
+    // building the buttons
+    UIButton *Button1 = [UIButton buttonWithType:UIButtonTypeCustom];
+    [Button1 setTitle:@"1" forState:UIControlStateNormal];
+    [Button1 setFrame:CGRectMake(20, 57, 280, 87)];
+    [Button1 setBackgroundImage:btn forState:UIControlStateNormal];
+    [Button1 setBackgroundImage:btnh forState:UIControlStateHighlighted];
+    
+    [Button1.titleLabel setFont:buttonFont];
+    [Button1 setTitleColor:buttonColorDefault forState:UIControlStateNormal];
+    [Button1 setTitleColor:buttonColorHighlight forState:UIControlStateHighlighted];
+    [Button1 addTarget:self action:@selector(pushMetrics:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:Button1];
+    
+    UIButton *Button2 = [UIButton buttonWithType:UIButtonTypeCustom];
+    [Button2 setTitle:@"2" forState:UIControlStateNormal];
+    [Button2 setFrame:CGRectMake(20, 157, 280, 87)];
+    [Button2 setBackgroundImage:btn forState:UIControlStateNormal];
+    [Button2 setBackgroundImage:btnh forState:UIControlStateHighlighted];
+    
+    [Button2.titleLabel setFont:buttonFont];
+    [Button2 setTitleColor:buttonColorDefault forState:UIControlStateNormal];
+    [Button2 setTitleColor:buttonColorHighlight forState:UIControlStateHighlighted];
+    [Button2 addTarget:self action:@selector(pushMetrics:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:Button2];
+    
+    UIButton *Button3 = [UIButton buttonWithType:UIButtonTypeCustom];
+    [Button3 setTitle:@"3" forState:UIControlStateNormal];
+    [Button3 setFrame:CGRectMake(20, 257, 280, 87)];
+    [Button3 setBackgroundImage:btn forState:UIControlStateNormal];
+    [Button3 setBackgroundImage:btnh forState:UIControlStateHighlighted];
+    
+    [Button3.titleLabel setFont:buttonFont];
+    [Button3 setTitleColor:buttonColorDefault forState:UIControlStateNormal];
+    [Button3 setTitleColor:buttonColorHighlight forState:UIControlStateHighlighted];
+    [Button3 addTarget:self action:@selector(pushMetrics:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:Button3];
+
 }
 
 - (void)didReceiveMemoryWarning
